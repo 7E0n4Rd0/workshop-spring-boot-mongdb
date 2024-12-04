d13d229f3b2fc0f1ab69b4af0e1a5e966bf2ad4c
leonArch <leomeireles66@gmail.com>
2024-12-04 07:04:50 -0300

    Inserting a User using POST

--- src/main/java/com/leon/workshopmong/services/UserService.java
+++ src/main/java/com/leon/workshopmong/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leon.workshopmong.domain.User;
+import com.leon.workshopmong.dto.UserDTO;
 import com.leon.workshopmong.repository.UserRepository;
 import com.leon.workshopmong.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object Not Found"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO dto) {
+		return new User(dto.getId(), dto.getName(), dto.getEmail());
 	}
 }
