d026ef26d9049f77f97f74820b89227b145362b4
leonArch <leomeireles66@gmail.com>
2024-12-04 08:05:19 -0300

    Projection of Author data with DTO

--- src/main/java/com/leon/workshopmong/config/Instantiation.java
+++ src/main/java/com/leon/workshopmong/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.leon.workshopmong.domain.Post;
 import com.leon.workshopmong.domain.User;
+import com.leon.workshopmong.dto.AuthorDTO;
 import com.leon.workshopmong.repository.PostRepository;
 import com.leon.workshopmong.repository.UserRepository;
 
@@ -37,8 +38,8 @@ public class Instantiation implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu Viagem","Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"),"Bom Dia","Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu Viagem","Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"),"Bom Dia","Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
