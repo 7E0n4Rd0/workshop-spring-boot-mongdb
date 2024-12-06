40efe9bceff4db77a3bf1b2bcf395f24bd62a98c
leonArch <leomeireles66@gmail.com>
2024-12-06 07:30:22 -0300

    A simple Query using annotation @Query

--- src/main/java/com/leon/workshopmong/repository/PostRepository.java
+++ src/main/java/com/leon/workshopmong/repository/PostRepository.java
@@ -3,9 +3,13 @@ package com.leon.workshopmong.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.leon.workshopmong.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	@Query("{'title': {$regex: ?0, $options: 'i'} }")
+	List<Post> findByTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
