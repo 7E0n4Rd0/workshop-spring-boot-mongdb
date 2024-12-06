988ec34d4a7cb16f9f6c99f7f8e7c271d09607ab
leonArch <leomeireles66@gmail.com>
2024-12-06 07:23:29 -0300

    simple query using Query Methods

--- src/main/java/com/leon/workshopmong/repository/PostRepository.java
+++ src/main/java/com/leon/workshopmong/repository/PostRepository.java
@@ -1,9 +1,11 @@
 package com.leon.workshopmong.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.leon.workshopmong.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
